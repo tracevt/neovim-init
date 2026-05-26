118068ff1d56b287bd5c3fef31beea28a947b4d6
Trace <trace@devcafe.mx>
2026-05-26 11:49:16 -0600

    Part 1 of becoming an indepent fork of NvChad
    This adds some default LSPs, Treesitter and Conform options for
    the default languages I used. This also uses the default
    NvChad options as the base but it doesn't import them from the
    library, instead it has them as a file that is used as the base

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "jsonls",
+  "gopls",
+  "ts_ls",
+  "eslint",
+  "rust_analyzer",
+  "ruby_lsp",
+  "pyright",
+  "ruff",
+}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
